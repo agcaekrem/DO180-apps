1980121d4df3a5243fafa4a805210ecd89d5b1c7
Ekrem <64022432+agcaekrem@users.noreply.github.com>
2023-05-17 16:07:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
    <title>PHP Test</title>
     </head>
      <body>
-      <?php echo "<p>Hello  World php version is " . phpversion() .'</p>'; ?>     
+      <?php echo "<p>Hello  World php version is " . phpversion() .'</p>'; ?>
+      <?php echo "<p>Update: App commit has been added.</p>"; ?>
        </body>
        </html>
